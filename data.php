76254f0fac9855dc228096e905add5a1d2b5707f
zalviandyr <zukronalviandy@gmail.com>
2022-08-04 15:57:02 +0700

    ini

---
+++ data.php
@@ -0,0 +1,196 @@
+<?php
+$mp1 = [
+    'Jalan Kaki', //0
+    'Sepeda', //1
+    'Sepeda Motor',
+]; //2
+$mp2 = [
+    'SD / sederajat', //0
+    'SMP / sederajat', //1
+    'SMA / sederajat',
+]; //2
+$mp3 = [
+    'Petani', //0
+    'Wiraswasta', //1
+    'Sudah Meninggal',
+]; //2
+$mp4 = [
+    'SD / sederajat', //0
+    'SMP / sederajat', //1
+    'SMA / sederajat',
+]; //2
+$mp5 = [
+    'Petani', //0
+    'PNS/TNI/POLRI', //1
+    'Tidak bekerja',
+]; //2
+$mp6 = [
+    'Kurang dari Rp. 500,000', //0
+    'Rp. 1,000,000 - Rp. 1,999,999', //1
+    'Rp. 2,000,000 - Rp. 4,999,999', //2
+    'Rp. 500,000 - Rp. 999,999', //3
+    'Tidak Berpenghasilan',
+]; //4
+
+$mp7 = [
+    'Disiplin', //0
+    'Tidak Disipilin', //1
+];
+
+$data = [];
+
+//1
+$d = [];
+$d['nama'] = 'ADITYA PUTRA ALMANDA';
+$d['p1'] = $mp1[1];
+$d['p2'] = $mp2[2];
+$d['p3'] = $mp3[1];
+$d['p4'] = $mp4[2];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[1];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//2
+$d = [];
+$d['nama'] = 'ADZIN MUHAMMAD ABIYYU';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[0];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[3];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//3
+$d = [];
+$d['nama'] = 'Agil Kalsa Putra';
+$d['p1'] = $mp1[2];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[0];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[2];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//4
+$d = [];
+$d['nama'] = 'PARNO RANDI';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[0];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[3];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//5
+$d = [];
+$d['nama'] = 'REFALDA RAMZI';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[2];
+$d['p3'] = $mp3[2];
+$d['p4'] = $mp4[2];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[4];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//6
+$d = [];
+$d['nama'] = 'PUTRI AMELLIZA';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[1];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[4];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//7
+$d = [];
+$d['nama'] = 'PUTRI RENO PRATAMA';
+$d['p1'] = $mp1[2];
+$d['p2'] = $mp2[2];
+$d['p3'] = $mp3[1];
+$d['p4'] = $mp4[2];
+$d['p5'] = $mp5[1];
+$d['p6'] = $mp6[3];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//8
+$d = [];
+$d['nama'] = 'TIARA EKA KEMBARA';
+$d['p1'] = $mp1[2];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[1];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[3];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//9
+$d = [];
+$d['nama'] = 'MELSI AGUSNI';
+$d['p1'] = $mp1[1];
+$d['p2'] = $mp2[0];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[0];
+$d['p5'] = $mp5[2];
+$d['p6'] = $mp6[0];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//10
+$d = [];
+$d['nama'] = 'RAHMI WIRDA YANTI';
+$d['p1'] = $mp1[2];
+$d['p2'] = $mp2[2];
+$d['p3'] = $mp3[2];
+$d['p4'] = $mp4[2];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[0];
+$d['p7'] = $mp7[1];
+$data[] = $d;
+
+//11
+$d = [];
+$d['nama'] = 'REGI ASIRGA DARLIS';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[1];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[1];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[0];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//12
+$d = [];
+$d['nama'] = 'REGO FITRA AGUS';
+$d['p1'] = $mp1[0];
+$d['p2'] = $mp2[1];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[1];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[2];
+$d['p7'] = $mp7[0];
+$data[] = $d;
+
+//13
+$d = [];
+$d['nama'] = 'M. IQBAL';
+$d['p1'] = $mp1[2];
+$d['p2'] = $mp2[1];
+$d['p3'] = $mp3[0];
+$d['p4'] = $mp4[0];
+$d['p5'] = $mp5[0];
+$d['p6'] = $mp6[1];
+$d['p7'] = $mp7[1];
+$data[] = $d;
